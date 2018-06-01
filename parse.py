7a5c4187b97d04da218db502e15ecb11da707f65
shuxin <shuxin@DN51vs1e.SUNet>
2018-06-01 01:01:14 -0700

    zylddzz

--- parse.py
+++ parse.py
@@ -25,10 +25,12 @@ def all_chord_types(files):
 
 def note_to_num_one(a):
     number = 0
+
     if (a[6][0] in translate.keys() and a[6][1] in adjust.keys()):
         note = translate[a[6][0]]
         adj = adjust[a[6][1]]
         #print(a[4], note, adj)
+
         return str(note + adj)
     if(a[6] == "note_root"): 
         return "note_root"
@@ -64,57 +66,129 @@ def chord_to_num_one(a):
             if chord in chordmap.keys():
                 note2 = (number + chordmap[chord][0] - 1) % 12 + 1
                 note3 = (number + chordmap[chord][1] - 1) % 12 + 1
-            #a[4] = [number, note2, note3]
-            a[4] = number
+            a[4] = [number, note2, note3]
+            #a[4] = number
     return str(a[4]) 
 
+def test(files):
+    c=0
+    printlist = []
 
-def makeVector(files):
-    c = 0
-    for file in files:
-        with open(file, 'r') as f:
-            reader = csv.reader(f)
-            all_list = list(reader)
-            remain = 0
-            song = []
-            for a in all_list:
-                a[4] = chord_to_num_one(a)
-                a[6] = note_to_num_one(a)
-                if(a[8] != 'note_duration'):
-                    count = float(a[8])
-                    if(remain != 0):
-                        count += remain
-                        remain = 0
-                    if(count == 1):
-                        song.append(a)
-                    while count > 1:
-                        a[8] = 1
-                        song.append(a)
-                        count -= 1
-                    if(count < 1):
-                       remain = count
-                #else: song.append(a)
-            with open(str(c)+'.csv', 'w', newline='') as myfile:
-                wr = csv.writer(myfile, delimiter=',')
-                for i in range(24):
-                    wr.writerow([0,0])
-                for a in song:
-                    wr.writerow([a[6], a[4]])
-                for i in range(24):
-                    wr.writerow([0,0])
+    with open(files, 'r') as f:
+        reader = csv.reader(f)
+        all_list = list(reader)
+        remain = 0
+        song = []
+
+
+        for a in all_list:
+            a[4] = chord_to_num_one(a)
+            a[6] = note_to_num_one(a)
+
+            if(a[8] != 'note_duration'):
+                count = float(a[8])
+                if(remain != 0):
+                    count += remain
+                    remain = 0
+                if(count == 1):
+                    song.append(a)
+                while count > 1:
+                    a[8] = 1
+                    song.append(a)
+                    count -= 1
+                if(count < 1):
+                   remain = count
+            #else: song.append(a)
+        with open(str(c)+'.csv', 'w', newline='') as myfile:
+            wr = csv.writer(myfile, delimiter=',')
+           # printlist.append([a[1][0],a[-1][1]])
+            # for i in range(24):
+            #     wr.writerow([0,0])
+            m = 1
+            testlist = []
+            for line in song:
+                # if (line[1][2] == "measure"):
+                #     return "meature"
+               # print(line[1])
+               # print(m)
+               # print(line[6])
+
+                if (int(line[1]) != m):
+                    #print(line[1], m)
+                    print(line)
+                    printlist.append((m, line[0],testlist,line[4]))#convert this to csv file
+                    testlist=[]
+                    m += 1
+                if(int(line[1]) == m):
+                    testlist.append(line[6])
+
+
+
+                #wr.writerow([list, a[4]])
+            # for i in range(24):
+            #     wr.writerow([0,0])
         c+=1
-    print(c)
+    print(printlist)
+
+# def makeVector(files):
+#     printlist = []
+#     c = 0
+#     for file in files:
+    #     with open(file, 'r') as f:
+    #         reader = csv.reader(f)
+    #         all_list = list(reader)
+    #         remain = 0
+    #         song = []
+    #         list=[]
+    #
+    #         for a in all_list:
+    #             a[4] = chord_to_num_one(a)
+    #             a[6] = note_to_num_one(a)
+    #
+    #             if(a[8] != 'note_duration'):
+    #                 count = float(a[8])
+    #                 if(remain != 0):
+    #                     count += remain
+    #                     remain = 0
+    #                 if(count == 1):
+    #                     song.append(a)
+    #                 while count > 1:
+    #                     a[8] = 1
+    #                     song.append(a)
+    #                     count -= 1
+    #                 if(count < 1):
+    #                    remain = count
+    #             #else: song.append(a)
+    #         with open(str(c)+'.csv', 'w', newline='') as myfile:
+    #             wr = csv.writer(myfile, delimiter=',')
+    #             printlist.append([a[1][-1],a[0][1]])
+    #             # for i in range(24):
+    #             #     wr.writerow([0,0])
+    #             m = 1
+    #             for a in song:
+    #                 if (a[1] == "measure"):
+    #                     return "meature"
+    #                 while a[1] == m:
+    #                     list.append(a[6])
+    #                 m += 1
+    #                 printlist.append(list,a[4])
+    #                 #wr.writerow([list, a[4]])
+    #             # for i in range(24):
+    #             #     wr.writerow([0,0])
+    #     c+=1
+    # print(printlist)
 
         
 
-path = 'csv_train'
+path = 'Dindi.csv'
 note = {}
 files = glob.glob(os.path.join(path, '*.csv'))
 
 #note_to_num(files)
 build_chord_map()
 #chord_to_num(files)
-makeVector(files)
+#makeVector(files)
+test(path)
 print(beat)
 
 
